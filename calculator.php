b64fd670bbbdd4dfada5cebb50b24bae7ac52b05
Arun Joseph <ajoseps@gmail.com>
2014-01-07 14:58:18 -0800

    in progress infix to postfix conversion

--- calculator.php
+++ calculator.php
@@ -3,7 +3,75 @@
 
 <?php
 $input = $_POST["calcInput"];
-print $input;
+$infixArray = preg_split("/([\+\-\/\*])/", $input, -1, PREG_SPLIT_DELIM_CAPTURE);
+$postfixArray = array();
+$stackArray = array();
+#infix to postfix conversion
+foreach($infixArray as $token)
+{
+	if(is_numeric($token))
+	{
+		array_push($postfixArray, $token);
+	}
+	else if($infixArray.size == 0)
+	{
+		while($stackArray.size != 0)
+		{
+			$remainingStackOperators = array_pop($stackArray);
+			array_push($postfixArray, $remainingStackOperators);
+		}
+	}
+	else
+	{
+		if($postfixArray.size == 0)
+		{
+			array_push($stackArray, $token);
+		}
+		else
+		{
+			# If stack is not empty, pop all greater/equal operators and add to postfix
+			$lastStackElement = $stackArray[$stackArray.size-1];
+
+			while(precedence($lastStackElement, $token))
+			{
+				$poppedStackElement = array_pop($stackArray);
+				array_push($postfixArray, $poppedStackElement);
+			}
+		}
+	}
+}
+
+foreach($postfixArray as $value)
+{
+	echo "IT'S NOT WORKING <br>";
+	echo "$value <br>";
+}
+
+# Assigns a value to the operator
+function precedenceValue($operator)
+{
+	switch($operator)
+	{
+		case "+":
+		case "-":
+			return 0;
+		case "*":
+		case "/":
+			return 1;
+		default:
+			return -1;
+	}
+}
+
+# Returns true if $operator1 has greater than or equal precedence to $operator2
+function precedence($operator1, $operator2)
+{
+	if($operator1 >= $operator2)
+		return true;
+	else
+		return false;
+}
+
 ?>
 
 </body>
