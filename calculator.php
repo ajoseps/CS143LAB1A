9d5249e786750a8112de54446516623ecbe26003
Arun Joseph <ajoseps@gmail.com>
2014-01-08 16:10:37 -0800

    most functionality is finished

--- calculator.php
+++ calculator.php
@@ -13,136 +13,22 @@
 $input = $_POST["calcInput"];
 $infixArray = preg_split("/([\+\-\/\*])/", $input, -1, PREG_SPLIT_DELIM_CAPTURE);
 $expressionStr = implode($infixArray);
-eval("\$result=".$expressionStr.";");
-echo "$input"."="."$result" ;
-/*
-$infixArray = preg_split("/([\+\-\/\*])/", $input, -1, PREG_SPLIT_DELIM_CAPTURE);
+$expressionStr = trim($expressionStr);
 
-# return input if there is less than 3 tokens (which is required for an operation)
-if(count(infixArray) < 3)
+if (preg_match('/[A-Za-z]/', $expressionStr))
 {
-	echo "Answer: $input";
+	echo "Invalid Expression: $expressionStr";
 }
 else
 {
-	$postfixArray = infixToPostfix($infixArray);
-	$resultEvaluation = postfixEval($postfixArray);
-	echo "Answer: $resultEvaluation";
-}
-
-foreach($postfixArray as $value)
-{
-	echo "$value <br>";
-}
-
-
-# evaluates the postfix expression
-function postfixEval($postfix)
-{
-	$stackArray = array();
-	foreach($postfix as $token)
+	if(strpos($expressionStr, "/0") == FALSE)
 	{
-		if(is_numeric($token))
-		{
-			array_push($stackArray, $token);
-		}
-		else
-		{
-			$poppedStackElement1 = array_pop($stackArray);
-			$poppedStackElement2 = array_pop($stackArray);
-			# might need to switch the $poppedStackElements around
-			$result = operationEval($poppedStackElement2, $poppedStackElement1, $token);
-			array_push($stackArray, $result);
-		}
+		eval("\$result=".$expressionStr.";");
 	}
-	return $stackArray[0];
+
+	echo "$input"."="."$result" ;
 }
 
-# evaluates an operation on two operands
-function operationEval($operand1, $operand2, $operator)
-{
-	switch($operator)
-	{
-		case "+":
-			return $operand1 + $operand2;
-		case "-":
-		    return $operand1 - $operand2;
-        case "*":
-        	return $operand1 * $operand2;
-		case "/":
-			return $operand1 / $operand2;
-		default:
-			return -1;
-	}
-}
-
-#infix to postfix conversion
-function infixToPostfix($infix)
-{
-	$postfix = array();
-	$stackArray = array();
-
-
-	foreach($infix as $token)
-	{
-		if(is_numeric($token))
-		{
-			array_push($postfix, $token);
-		}
-		else
-		{
-			if(count($stackArray) == 0)
-			{
-				array_push($stackArray, $token);
-			}
-			else
-			{
-				# If stack is not empty, pop all greater/equal operators and add to postfix
-				$lastStackElement = $stackArray[count($stackArray) -1];
-				while(precedence($lastStackElement, $token))
-				{
-					$poppedStackElement = array_pop($stackArray);
-					array_push($postfix, $poppedStackElement);
-					$lastStackElement = $stackArray[count($stackArray) -1];
-				}
-				array_push($stackArray, $token);
-			}
-		}
-	}
-	while(count($stackArray) != 0)
-	{
-		$remainingStackOperators = array_pop($stackArray);
-		array_push($postfix, $remainingStackOperators);
-	}
-	return $postfix;
-}
-# Assigns a value to the operator
-function precedenceValue($operator)
-{
-	switch($operator)
-	{
-		case "+":
-		case "-":
-		    return 0;
-        case "*":
-		case "/":
-			return 1;
-		default:
-			return -1;
-	}
-}
-
-# Returns true if $operator1 has greater than or equal precedence to $operator2
-function precedence($operator1, $operator2)
-{
-    $precedenceop1 = precedenceValue($operator1);
-    $precedenceop2 = precedenceValue($operator2);
-	if($precedenceop1 >= $precedenceop2)
-		return true;
-	else
-		return false;
-}
-*/
 ?>
 
 </body>
